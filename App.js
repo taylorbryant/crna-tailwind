f7e3d618fea3ab777d61c1f1fca7b477a3b70af8
Taylor Bryant <taylor@bryant.io>
2018-03-24 01:15:17 -0500

    Add Tailwind usage example

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import styles from "./components/styles";
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
+      <View
+        style={[
+          styles.flex_1,
+          styles.bg_white,
+          styles.item_center,
+          styles.justify_center
+        ]}
+      >
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
@@ -12,12 +20,3 @@ export default class App extends React.Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
